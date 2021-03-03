b70efb69426b4169ae41fdddea86ef1de189ed3d
loanl <loanlemauff@outlook.fr>
2021-03-03 17:33:06 +0100

    adding login/register forms

--- app/src/pages/home-page/HomePage.tsx
+++ app/src/pages/home-page/HomePage.tsx
@@ -1,15 +1,30 @@
 import React, { FunctionComponent } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Button } from 'react-native';
 
 /**
  * The react home page.
  */
 
-const HomePage: FunctionComponent = () => {
+// const HomePage: FunctionComponent = () => {
 
+//   return (
+//     <View>
+//       <Text>Home Page</Text>
+//     </View>
+//   );
+// }
+
+function HomePage({ navigation }) {
   return (
-    <View>
-      <Text>Home Page</Text>
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Button
+        title="Se connecter"
+        onPress={() => navigation.navigate('Login')}
+      />
+      <Button
+        title="S'enregistrer"
+        onPress={() => navigation.navigate('Register')}
+      />
     </View>
   );
 }
