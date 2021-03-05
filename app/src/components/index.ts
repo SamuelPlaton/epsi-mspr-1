15b70a7037905b53032bf0f38445bc8b459e1390
loanl <loanlemauff@outlook.fr>
2021-03-05 10:21:36 +0100

    outsource files from login/register page

--- app/src/components/index.ts
+++ app/src/components/index.ts
@@ -1,2 +1,7 @@
 export * from './cards';
+<<<<<<< HEAD
 export * from './info';
+=======
+export { default as LoginForm } from './form/LoginForm';
+export { default as RegisterForm } from './form/RegisterForm';
+>>>>>>> outsource files from login/register page
--- app/src/pages/login-register-page/LoginPage.tsx
+++ app/src/pages/login-register-page/LoginPage.tsx
@@ -1,92 +1,13 @@
-import React, { Component } from 'react';
-import { Alert, View, Text, Button, TextInput, StyleSheet, TouchableOpacity } from 'react-native';
+import React, { FunctionComponent } from 'react';
+import { LoginForm } from "../../components";
 
-/**
- * Login page.
- */
-
-export default class Login extends Component {
-    state = {
-    email: '',
-    password: '',
-  };
+const LoginPage:FunctionComponent = () => {
+  return (
+    <LoginForm></LoginForm>
+  );
+}
 
 
-    onLogin() {
-        const { email, password } = this.state;
-        Alert.alert('Informations', `email: ${email} + Mot de passe: ${password}`);
-    }
-
-    render() {  
-    return (
-        <View style={styles.container}>
-        <Text style={styles.titleText}>Se connecter</Text>
-          <TextInput
-            value={this.state.email}
-            keyboardType = 'email-address'
-            onChangeText={(email) => this.setState({ email })}
-            placeholder='email'
-            placeholderTextColor = 'white'
-            style={styles.input}
-          />
-          <TextInput
-            value={this.state.password}
-            onChangeText={(password) => this.setState({ password })}
-            placeholder={'Mot de passe'}
-            secureTextEntry={true}
-            placeholderTextColor = 'white'
-            style={styles.input}
-          />
-          
-       
-          <TouchableOpacity
-            style={styles.button}
-            onPress={this.onLogin.bind(this)}
-         >
-           <Text style={styles.buttonText}> Connexion </Text>
-         </TouchableOpacity>
-          
-        </View>
-      );
-    }
-}   
 
 
-const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      alignItems: 'center',
-      justifyContent: 'center',
-      backgroundColor: 'grey',
-    },
-    titleText:{
-      fontSize: 50,
-      alignItems: 'center',
-      justifyContent: 'center',
-    },
-    button: {
-      alignItems: 'center',
-      backgroundColor: 'powderblue',
-      width: 200,
-      height: 44,
-      padding: 10,
-      borderWidth: 1,
-      borderColor: 'white',
-      borderRadius: 25,
-      marginBottom: 10,
-    },
-    buttonText:{
-      fontSize: 20,
-      alignItems: 'center',
-      justifyContent: 'center',
-    },
-    input: {
-      width: 200,
-      fontSize: 20,
-      height: 44,
-      padding: 10,
-      borderWidth: 1,
-      borderColor: 'white',
-      marginVertical: 10,
-    },
-  });
+export default LoginPage;
