67e9b1d52f8a555fe9222b38c242038aee1679f2
LorenzoBorda <lorenzo.borda@epsi.fr>
2021-03-31 21:45:40 +0200

    UX update

--- app/src/components/navigation/burgermenu/BurgerMenu.tsx
+++ app/src/components/navigation/burgermenu/BurgerMenu.tsx
@@ -1,16 +1,40 @@
-import React, {FunctionComponent} from 'react';
-import {Image, Text, TouchableOpacity, StyleSheet, Button, View} from 'react-native';
+import React, {FunctionComponent, useEffect, useState} from 'react';
+import {Image, Text, Button, TouchableOpacity, StyleSheet, View} from 'react-native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import {User} from '../../../entities';
 
 import {useRoute, useNavigation} from '@react-navigation/native';
 import { genericStyles } from '../../../styles';
 
+import { retrieveActiveUser } from '../../../store/UserManager';
+
 const BurgerMenu: FunctionComponent = () => {
 
-    var isActive = false;
-
     const nav = useNavigation();
-
     const route = useRoute();
+
+    const [activeUser, setActiveUser] = useState<User | undefined>(route.params ? (route.params as User) : undefined);
+
+    const getData = async () => {
+        if (!activeUser) {
+            retrieveActiveUser().then((response) => {
+              if (response) {
+                setActiveUser(response);
+              }
+            });
+          }
+    }
+
+    const disconnect = async () => {
+        await AsyncStorage.removeItem('activeUser', () => nav.navigate('Home'));
+
+    }
+
+    useEffect(() => {
+        getData();
+      }, [activeUser]);
+
+    const [isActive, setActive] = useState(false);
 
     const styles = StyleSheet.create({
         cont: {
@@ -35,10 +59,10 @@ const BurgerMenu: FunctionComponent = () => {
             color: 'black'
         },
         unactive: {
-            display: "none"
+              display: "none"
         },
         blur: {
-            backgroundColor: 'rgba(0,0,0,0.5)',
+            backgroundColor: 'rgba(52,52,52,0.5)',
             width: "100%",
             height: "100%"
         },
@@ -61,10 +85,6 @@ const BurgerMenu: FunctionComponent = () => {
         }
     });
 
-    const popSidebar = () => {
-        isActive = !isActive;
-    }
-  
     return (
         <View style={styles.cont}>
             <View style={styles.topbar}>
@@ -73,7 +93,7 @@ const BurgerMenu: FunctionComponent = () => {
                 </TouchableOpacity>
                 <Text style={{marginLeft: 30}}>{route.name}</Text>
                 <View style={isActive ? styles.buttonActive : styles.buttonUnactive}>
-                    <TouchableOpacity activeOpacity={1} onPress={() => popSidebar()}>
+                    <TouchableOpacity activeOpacity={1} onPress={() => setActive(!isActive)}>
                         <Image style={styles.tinyIcon} source={require('../../../assets/icons/menu.png')}/>
                     </TouchableOpacity>
                 </View>
@@ -84,21 +104,35 @@ const BurgerMenu: FunctionComponent = () => {
                         <TouchableOpacity activeOpacity={1} onPress={() => nav.navigate('Home')}>
                             <Text style={styles.text}>Home</Text>
                         </TouchableOpacity>
-                        <TouchableOpacity activeOpacity={1} onPress={() => nav.navigate('Scan')}>
-                            <Text style={styles.text}>QR scanner</Text>
-                        </TouchableOpacity>
-                        <TouchableOpacity activeOpacity={1} onPress={() => nav.navigate('Coupons')}>
-                            <Text style={styles.text}>Coupons</Text>
-                        </TouchableOpacity>
-                        <TouchableOpacity activeOpacity={1} onPress={() => nav.navigate('Historic')}>    
-                            <Text style={styles.text}>Historique</Text>
-                        </TouchableOpacity>
-                        <TouchableOpacity activeOpacity={1} onPress={() => nav.navigate('Map')}>
-                            <Text style={styles.text}>Mappe</Text>
-                        </TouchableOpacity>
-                        <TouchableOpacity activeOpacity={1} onPress={() => nav.navigate('User')}>    
-                            <Text style={styles.text}>Profile</Text>
-                        </TouchableOpacity>
+                        {activeUser ? (
+                            <View>
+                                <TouchableOpacity activeOpacity={1} onPress={() => nav.navigate('Scan')}>
+                                    <Text style={styles.text}>QR scanner</Text>
+                                </TouchableOpacity>
+                                <TouchableOpacity activeOpacity={1} onPress={() => nav.navigate('Coupons')}>
+                                    <Text style={styles.text}>Coupons</Text>
+                                </TouchableOpacity>
+                                <TouchableOpacity activeOpacity={1} onPress={() => nav.navigate('Historic')}>    
+                                    <Text style={styles.text}>Historique</Text>
+                                </TouchableOpacity>
+                                <TouchableOpacity activeOpacity={1} onPress={() => nav.navigate('Map')}>
+                                    <Text style={styles.text}>Mappe</Text>
+                                </TouchableOpacity>
+                                <TouchableOpacity activeOpacity={1} onPress={() => nav.navigate('User')}>    
+                                    <Text style={styles.text}>Profile</Text>
+                                </TouchableOpacity>
+                                <TouchableOpacity style={{backgroundColor: "red", position: "absolute", bottom: 10, left: 10}} onPress={async () => disconnect()}>Déconnexion</TouchableOpacity>
+                            </View>
+                            ) : (
+                            <View>
+                                <TouchableOpacity activeOpacity={1} onPress={() => nav.navigate('Login')}>    
+                                    <Text style={styles.text}>Se connecter</Text>
+                                </TouchableOpacity>
+                                <TouchableOpacity activeOpacity={1} onPress={() => nav.navigate('Register')}>    
+                                    <Text style={styles.text}>S'enregister</Text>
+                                </TouchableOpacity> 
+                            </View>
+                            )}
                     </View>
                 </View>
             </View>
